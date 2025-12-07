6d3e2f6f35a799ea84dc3ae99c51560396b38b23
umangkumar-oodles <umang.kumar@oodles.io>
2025-12-08 00:51:56 +0530

    add log

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDatabase = async () => {
     try {
+        console.log('checking the MONGO_URI',process.env.MONGO_URI)
         const conn = await mongoose.connect(process.env.MONGO_URI!);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
